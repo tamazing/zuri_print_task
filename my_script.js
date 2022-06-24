f08b45443455714377c9c0be9bf7929158542c91
tamazing <taiwolasaki@gmail.com>
2022-06-24 08:58:42 +0100

    I changed the method used to print my details to document.write

--- my_script.js
+++ my_script.js
@@ -2,9 +2,5 @@ const name = "Lasaki Taiwo"
 let height = 1.75
 const country = "Nigeria"
 
-let my_details = "My name is "+ name + ", I am "+ height+" meters tall and I am from "+ country
-
-document.getElementById("screen_print").innerHTML = my_details
-
-console.log("\n My name is "+ name + ", I am "+ height+" meters tall and I am from "+ country + ".\n")
+document.write("My name is "+ name + ", I am "+ height+" meters tall and I am from "+ country)
 
